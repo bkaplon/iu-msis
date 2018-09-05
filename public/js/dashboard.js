6b4acfda5faa8ada032c5f7b82a63e101d77b7b9
Tom G <tomg@byu.net>
2018-09-04 21:41:40 -0400

    Add 'tasks' key (null for now); name cleanup.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,26 +1,30 @@
 /**
  *
  **/
-var app = new Vue({
-  el: '#app',
+var dashboardChartsApp = new Vue({
+  el: '#topCharts',
   data: {
-    project: {
-      "name" : "Tapestry",
-      "short_description": "Build a visualization layer for the project dashboard",
-      "start_date" : "2018-07-01",
-      "target_date" : "2018-11-03",
-      "budget" : "4950000",
-      "spent" : "3456700",
-      "projected_spend": "4740500",
-      "weekly_effort_target": 400
+      name : "Tapestry",
+      short_description: "Build a visualization layer for the project dashboard",
+      start_date : "2018-07-01",
+      target_date : "2018-11-03",
+      budget : "4950000",
+      spent : "3456700",
+      projected_spend: "4740500",
+      weekly_effort_target: 400,
+      tasks: null
+  },
+  computed: {
+    target_date_pretty: function () {
+      return this.prettyDate(this.target_date)
+    },
+    days_left: function () {
+      return moment(this.target_date).diff(moment(), 'days')
     }
   },
   methods: {
     prettyDate: function (d) {
       return moment(d).format('l')
-    },
-    daysLeft: function (d) {
-      return moment(d).diff(moment(), 'days')
     }
   }
   // ,
