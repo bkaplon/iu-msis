6059e2b8f73cf8282bbe603ee0db78ffd0a32093
Tom G <tomg@byu.net>
2018-09-07 13:39:21 -0400

    End video 6b Binding attributes. Logic for task tr|td classes

--- public/js/dashboard.app.js
+++ public/js/dashboard.app.js
@@ -87,6 +87,14 @@ var dashboardApp = new Vue({
       }
 
       return '$ ' + (val/1e6).toFixed(1) + ' M'
+    },
+    completeClass: function(task) {
+      if (task.perc_complete == 100 ) {
+        return 'alert-success'
+      }
+      if (task.current_sprint && task.hours_worked == 0 ) {
+        return 'alert-warning'
+      }
     }
   }
 })
