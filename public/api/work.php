c52909fa5862123141f4ff88d5389bc5df70a346
Tom G <tomg@byu.net>
2018-09-24 16:14:19 -0400

    End of class 9/24 (really)

--- public/api/work.php
+++ public/api/work.php
@@ -1,58 +1,12 @@
 <?php
 
-echo '<pre>';
-echo "foo\n";
-echo 'bar\n';
+require '../../app/common.php';
 
-$name = 'Phil';
+// Get the taskId from URL params
+$taskId = intval($_GET['taskId'] ?? 0);
 
-echo "\n";
-echo "My name is {$name}\n";
-echo "My name is " . $name . "\n";
+// Fetch the work from the db
+$work = Work::findByTaskId($taskId);
 
-class Animal
-{
-  public $type = 'Pig';
-}
-
-$wilbur = new Animal();
-echo $wilbur->type;
-
-if (true) {
-  // This
-} else {
-  // That
-}
-
-for ($i = 0; $i< 10; $i++) {
-  // stuff
-}
-
-$id = 0;
-if (isset($_GET['id'])) {
-  $id = $_GET['id'];
-}
-
-$id = isset($_GET['id']) ? $_GET['id'] : 0;
-
-$id = $_GET['id'] ?? 0;
-
-
-$arr1 = [
-  'first' => 'Tom',
-  'last' => 'Gregory'
-];
-
-echo '[0]:' . $arr1[0]; // Won't work!
-echo '[first]:' . $arr1['first']; // Will work
-
-$arr2 = [ 'one', 'two', 'three'];
-
-$arr2 = [
-  0 => 'one',
-  1 => 'two',
-  2 => 'three'];
-
-foreach($arr2 as $key=>$val) {
-  echo $key .':'.$val;
-}
+// convert to json and print
+echo json_encode($work);
