dbd96a364f9aaf0674f39693b94bf24e030e4724
Tom G <tomg@byu.net>
2018-09-24 08:43:32 -0400

    Work api starter

--- app/models/Work.php
+++ app/models/Work.php
@@ -3,6 +3,9 @@
 class Work
 {
   public static function findByTaskId($id) {
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    var_dump($db);
+
     die;
   }
 }
--- public/api/work.php
+++ public/api/work.php
@@ -1,6 +1,15 @@
 <?php
 
+require '../../app/common.php';
+
 // POST only, as work is normally listed with tasks
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
      // The request is using the POST method
 }
+
+// ELSE, get
+$taskId = $GET['taskId'] ?? 0;
+
+$work = Work::findByTaskId($taskId);
+
+echo json_encode($work);
