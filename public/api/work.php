d5a3d7f1121dd0324ec956e3473ce84f55ee1620
Tom G <tomg@byu.net>
2018-09-24 12:53:25 -0400

    In class 9/24

--- public/api/work.php
+++ public/api/work.php
@@ -1,58 +1,12 @@
 <?php
 
-echo '<pre>';
+require '../../app/common.php';
 
-$foo = "bar\n";
-$bar = 'bit\n';
+// Get the taskId
+$taskId = $_GET['taskId'] ?? 0;
 
-echo $foo;
-echo $bar;
+// Fetch Work from database
+$work = Work::findByTaskId($taskId);
 
-$one = 'ONE';
-
-echo "The value is {$one}.\n";
-echo "The value is " . $one;
-
-class Animal
-{
-  public $name = 'Pig';
-}
-
-$wilber = new Animal();
-echo $wilber->name . "\n";
-
-
-$myArr = [
-
-  'first' => 'Tom',
-  'last' => 'Gregory'
-
-];
-
-$people = [
-
-  0 => 'Tom',
-  1 => 'Phil',
-  2 => 'Midge'
-
-];
-
-var_dump($myArr);
-print_r($people);
-
-/*
-$n = 0;
-
-if (isset($_GET['id'])) {
-  $n = $_GET['id']
-}
-
-// This means the same thing:
- $n = isset($_GET['id']) ? $_GET['id'] : 0;
-*/
-
- // This also means the same thing:
-$n = $_GET['id'] ?? 0;
-
-
-echo '$n = ' . $n ."\n";
+// convert to JSON and print
+echo json_encode($work);
