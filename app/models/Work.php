b56633136d7c104aa37592e7c612e035c35cefc7
Tom G <tomg@byu.net>
2018-10-01 13:10:36 -0400

    In class 10/1 Work post

--- app/models/Work.php
+++ app/models/Work.php
@@ -11,7 +11,7 @@ class Work
   public $completion_estimate;
 
   public function __construct($row) {
-    $this->id = intval($row['id']);
+    $this->id = isset($row['id']) ? intval($row['id']) : null;
 
     $this->task_id = intval($row['task_id']);
     $this->team_id = intval($row['team_id']);
@@ -29,6 +29,24 @@ class Work
     $this->stop = $date->format('Y-m-d H:i:s');
 
     $this->completion_estimate = intval($row['completion_estimate']);
+  }
+
+  public function create() {
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+
+    $sql = 'INSERT INTO Work (team_id, task_id, start_date, hours, completion_estimate)
+            VALUES (?,?,?,?,?)';
+
+    $statement = $db->prepare($sql);
+    $success = $statement->execute([
+      $this->team_id,
+      $this->task_id,
+      $this->start,
+      $this->hours,
+      $this->completion_estimate
+    ]);
+
+    $this->id = $db->lastInsertId();
   }
 
   public static function getWorkByTaskId(int $taskId) {
