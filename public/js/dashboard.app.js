ebb617e621f6a4d6ec50b3a704f1f3557022291a
Tom G <tomg@byu.net>
2018-10-15 10:00:10 -0400

    After class 10/15: effort chart

--- public/js/dashboard.app.js
+++ public/js/dashboard.app.js
@@ -80,7 +80,7 @@ var dashboardApp = new Vue({
       .then( json => {
         dashboardApp.workHours = json;
         this.formatWorkHours();
-        // this.buildEffortChart();
+        this.buildEffortChart();
       })
       .catch( err => {
         console.log('PROJECT HOURS FETCH ERROR:');
@@ -98,6 +98,60 @@ var dashboardApp = new Vue({
         }
       );
       console.log(this.workHours);
+    },
+    buildEffortChart() {
+      Highcharts.chart('effortChart', {
+            chart: {
+                zoomType: 'x'
+            },
+            title: {
+                text: 'Cumulative Effort'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'Hours'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
+            series: [{
+                type: 'area',
+                name: 'Effort (hrs)',
+                data:  this.workHours.map( entry =>
+                  [entry.date, entry.runningTotalHours]
+                ) //Expects [ [date1, val1], [date2, val2], [] ]
+            }]
+        });
     },
     gotoTask(tid) {
       window.location = 'task.html?taskId=' + tid;
